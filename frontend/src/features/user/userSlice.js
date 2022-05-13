699597341eef0b17f9bffa57cdd1eb32d5b064a9
Piyush Aggarwal <27003616+ThePiyushAggarwal@users.noreply.github.com>
2022-05-13 17:04:56 +0530

    frontend form. mock register

---
+++ frontend/src/features/user/userSlice.js
@@ -0,0 +1,51 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import userService from './userService'
+
+const initialState = {
+  user: {},
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: false,
+}
+
+export const registerUser = createAsyncThunk(
+  'user/register',
+  async (userData, thunkAPI) => {
+    try {
+      return await userService.registerUser(userData)
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString()
+      return thunkAPI.rejectWithValue(message)
+    }
+  }
+)
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.isLoading = true
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.isLoading = false
+      state.user = payload
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.isLoading = false
+      state.isError = true
+      state.message = payload
+    },
+  },
+})
+
+// export const {} = userSlice.actions
+
+export default userSlice.reducer
